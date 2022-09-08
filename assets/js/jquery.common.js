588af1b88d99709d0442e3a7467464e47ed00c28
quanghuy <huynguyenquang@tcom.vn>
2022-09-08 17:39:38 +0700

    a

--- assets/js/jquery.common.js
+++ assets/js/jquery.common.js
@@ -248,8 +248,8 @@ jQuery(function() {
         slidesToShow: 1,
         dots: true,
         arrows: true,
-        prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><img src="../../../assets/img/common/icn_left.svg" class="slide-arrow prev-arrow"></button>',
-        nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"><img src="../../../assets/img/common/icn_right.svg" class="slide-arrow prev-arrow"></button>',
+        prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><img src="./assets/img/common/icn_left.svg" class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"><img src="./assets/img/common/icn_right.svg" class="slide-arrow prev-arrow"></button>',
         responsive: [{
                 breakpoint: 600,
                 settings: {
